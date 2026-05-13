ded9fac3a431b77010a11dfbb1b32ad12cff3371
MaviE <424000472@ntc.edu.ph>
2026-05-13 20:27:28 +0800

    Update DatabaseConnection.vb

--- CODE/FloodBuster/Data Access Layer/DatabaseConnection.vb
+++ CODE/FloodBuster/Data Access Layer/DatabaseConnection.vb
@@ -1,1 +1,13 @@
+Imports System.Data.SqlClient
+
+Public Class DatabaseConnection
+
+    Private Const CONNECTION_STRING As String = "Server=MAVI\SQLEXPRESS;Database=FloodBusterDB;Integrated Security=True;TrustServerCertificate=True;"
+
+    Public Shared Function GetConnection() As SqlConnection
+        Return New SqlConnection(CONNECTION_STRING)
+    End Function
+
+End Class
+
 
