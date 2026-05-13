f823ea7d16e421085da01606efb59eb181278bf7
MaviE <424000472@ntc.edu.ph>
2026-05-13 20:23:39 +0800

    Update EvacuationService.vb

--- CODE/FloodBuster/Business Logic Layer/EvacuationService.vb
+++ CODE/FloodBuster/Business Logic Layer/EvacuationService.vb
@@ -1,1 +1,20 @@
+Public Class EvacuationService
 
+    Private _evacRepo As New EvacuationRepository()
+
+    ' Get all barangays for the dropdown
+    Public Function GetAllBarangays() As DataTable
+        Return _evacRepo.GetAllBarangays()
+    End Function
+
+    ' Get evacuation path recommendations for a selected barangay
+    Public Function GetRecommendations(barangayName As String) As DataTable
+        If String.IsNullOrWhiteSpace(barangayName) Then
+            MessageBox.Show("Please select a barangay.", "Validation Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return New DataTable()
+        End If
+        Return _evacRepo.GetPathRecommendations(barangayName)
+    End Function
+
+End Class
